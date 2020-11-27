1f9bf869a70bfdf6061485967213afa7bd9c2345
Oskar Zaremba <oskar.zaremba@gmail.com>
2020-11-27 13:23:15 +0100

    Added Row Selection Functionality

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
-var dataSet;
+var currenciesData;
+var selectedCurrency;
+var selectedCurrencyData;
 
 $.ajax({
     type: 'GET',
     url: 'https://api.exchangeratesapi.io/latest?base=PLN',
     dataType: 'json',
     success: function (data) {
-        dataSet = Object.entries(data.rates);
+        currenciesData = Object.entries(data.rates);
     }
 });
 
 $('button#action').click(function () {
-    console.log(dataSet);
+    console.log(currenciesData);
 });
 
 $(document).ready(function () {
-    $('#exchange_rates').DataTable({
+    var exchangeRatesTable = $('#exchange_rates').DataTable({
         dom: 'rt',
-        data: dataSet,
+        data: currenciesData,
         columns: [
             { title: "Currency" },
             { title: "Exchange Rate" }
         ],
         scrollY: 600,
         paging: false
+    });
+    $('#exchange_rates tbody').on('click', 'tr', function () {
+        var rowData = exchangeRatesTable.row(this).data();
+        selectedCurrency = rowData[0]
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+        }
+        else {
+            exchangeRatesTable.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
     });
 });
