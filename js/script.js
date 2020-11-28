315e34ce472feb829537c0c1e4229ebb54ddbd4f
Oskar Zaremba <oskar.zaremba@gmail.com>
2020-11-28 10:51:28 +0100

    Requested functions are working

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-var currenciesData;
-var selectedCurrency;
-var selectedCurrencyData;
+var currencyRateUrl;
+var startDate = getPreviousWeekMonday();
+var endDate = getTodayDate();
 
-$.ajax({
-    type: 'GET',
-    url: 'https://api.exchangeratesapi.io/latest?base=PLN',
-    dataType: 'json',
-    success: function (data) {
-        currenciesData = Object.entries(data.rates);
-    }
-});
+function getTodayDate() {
+    return new Date().toISOString().split('T')[0];
+}
 
-$('button#action').click(function () {
-    console.log(currenciesData);
-});
+function getPreviousWeekMonday() {
+    var today = new Date();
+    var mondayLastWeek = new Date();
+    var currentDayOfWeek = new Date().getDay() === 0 ? 7 : new Date().getDay();
+    var daysToSubstruct = (currentDayOfWeek - 1) + 7;
+    mondayLastWeek.setDate(today.getDate() - daysToSubstruct);
+    return mondayLastWeek.toISOString().split('T')[0];
+}
 
 $(document).ready(function () {
-    var exchangeRatesTable = $('#exchange_rates').DataTable({
+
+    var currenciesRatesTable = $('#currencies_rates').DataTable({
         dom: 'rt',
-        data: currenciesData,
+        ajax: {
+            url: 'https://api.exchangeratesapi.io/latest?base=PLN',
+            type: 'GET',
+            dataSrc: function (data) {
+                return Object.keys(data.rates).map(function (key) { return [key, (1 / data.rates[key]).toFixed(4)] });
+            }
+        },
         columns: [
             { title: "Currency" },
             { title: "Exchange Rate" }
@@ -26,15 +33,42 @@ $(document).ready(function () {
         scrollY: 600,
         paging: false
     });
-    $('#exchange_rates tbody').on('click', 'tr', function () {
-        var rowData = exchangeRatesTable.row(this).data();
+
+    var currencyRateTable = $('#currency_rates').DataTable({
+        dom: 'rt',
+        ajax: {
+            url: currencyRateUrl,
+            type: 'GET',
+            dataSrc: function (data) {
+                var historicalData = data.rates;
+                return Object.keys(historicalData).map(function (date) {
+                    dataInDate = historicalData[date];
+                    return Object.keys(dataInDate).map(function (currency) {
+                        return [date, (1 / dataInDate[currency]).toFixed(4)];
+                    }).flat();
+                });
+            }
+        },
+        columns: [
+            { title: "Date" },
+            { title: "Exchange Rate" }
+        ],
+        scrollY: 600,
+        paging: false
+    });
+
+    $('#currencies_rates tbody').on('click', 'tr', function () {
+        var rowData = currenciesRatesTable.row(this).data();
         selectedCurrency = rowData[0]
+        currencyRateUrl = 'https://api.exchangeratesapi.io/history?start_at=' + startDate + '&end_at=' + endDate + '&base=PLN&symbols=' + selectedCurrency;
+        currencyRateTable.ajax.url(currencyRateUrl).load();
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
         }
         else {
-            exchangeRatesTable.$('tr.selected').removeClass('selected');
+            currenciesRatesTable.$('tr.selected').removeClass('selected');
             $(this).addClass('selected');
         }
     });
+
 });
